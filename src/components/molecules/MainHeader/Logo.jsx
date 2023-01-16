1e996f142a8166a829e806f170105a6c4c1a5b1c
JosephJS123 <josephangelo_guerrero@hotmail.com>
2023-01-16 18:28:27 -0500

    Se agregar router, links al menú y la configuración inicial de TailwindCSS

---
+++ src/components/molecules/MainHeader/Logo.jsx
@@ -0,0 +1,11 @@
+import logo from "../../../assets/logo.png";
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={logo} alt="Logo eCommerce" />
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/components/organisms/MainHeader.jsx
@@ -0,0 +1,13 @@
+import Logo from "../molecules/MainHeader/Logo";
+import MainMenu from "../molecules/MainHeader/MainMenu";
+
+const MainHeader = () => {
+  return (
+    <div>
+      <Logo />
+      <MainMenu />
+    </div>
+  );
+};
+
+export default MainHeader;
